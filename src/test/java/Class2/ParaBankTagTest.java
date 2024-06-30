fab0d65bb63a4c6db272878f74ebf1d2dbf85284
Ragib <ragib2789@gmail.com>
2024-06-30 21:08:13 +0600

    Assignment at ParaBank on TagTest

--- src/test/java/Class2/ParaBankTagTest.java
+++ src/test/java/Class2/ParaBankTagTest.java
@@ -8,30 +8,27 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.List;
 
 public class ParaBankTagTest {
-
     public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver=new FirefoxDriver();
         driver.get("https://parabank.parasoft.com/parabank/index.htm;jsessionid=23E470A0D6E69946BA23D2013959D91E");
         driver.manage().window().maximize();
+
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
 
 
-        driver.findElements(By.tagName("a")
-
-                /* Vaiya Facing Problem By getting tag name operation // */
-
+        List<WebElement> webElements = driver.findElements(By.tagName("a"));
+        System.out.println("count: " +webElements.size());
+        for (WebElement element:webElements){
+            System.out.println(element.getText()+ " "+element.getAttribute("href"));
         }
+        /*
+        *getText() <a>text under anchor</a>  return:text under anchor
+        getattribute("id") <a id="usingID">text under anchor </a>  Return:Using ID
+        */
+
+        driver.quit();
 
 
     }
-
-
-
-
-
-
-
 }
-
-
