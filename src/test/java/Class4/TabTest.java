d6c510157f475c78ec082376a0507e8c2673b5f4
Ragib <ragib2789@gmail.com>
2024-08-01 16:36:48 +0800

    Assignments for class 3 & 4

--- src/test/java/Class4/TabTest.java
+++ src/test/java/Class4/TabTest.java
@@ -2,8 +2,13 @@ package Class4;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class TabTest {
     public static void main (String [] args) throws InterruptedException {
@@ -12,7 +17,16 @@ public class TabTest {
         driver.get("https://demoqa.com/browser-windows");
         driver.manage().window().maximize();
 
+
+       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));  Implicitly wait //
+
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(30));
+
+
         String parentPage=driver.getWindowHandle();
+        driver.getWindowHandles();
+        WebElement element=driver.findElement(By.className("text-center"));
+        wait.until(ExpectedConditions.visibilityOf(element));
         System.out.println("Main Window :"+driver.findElement(By.className("text-center")).getText());
 
         Thread.sleep(2000);
