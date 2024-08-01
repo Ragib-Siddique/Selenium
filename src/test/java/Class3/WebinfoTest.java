8c588652a22dc7a3b36cd66383304665bb337961
Ragib <ragib2789@gmail.com>
2024-08-01 17:04:21 +0800

    Assignments for class 3 & 4

--- src/test/java/Class3/WebinfoTest.java
+++ src/test/java/Class3/WebinfoTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class WebonfoTest {
+public class WebinfoTest {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver=new FirefoxDriver();
         driver.get("https://www.saucedemo.com/");
